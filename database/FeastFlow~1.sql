f98243ac5f64ac23a925f86fab0c49a1f82e2d28
Shohana Akter Rabina <shohana592059@gmail.com>
2026-06-25 19:06:35 +0600

    create dropping file separately
    Removed multiple drop statements for tables and sequences, and added table and sequence creation statements for users, categories, menu_items, restaurant_tables, orders, order_items, payments, staff, inventory, menu_item_ingredients, feedback, and audit_log.

--- database/FeastFlow~1.sql
+++ database/FeastFlow~1.sql
@@ -1,31 +1,3 @@
--- dropping table
-drop table audit_log;
-drop table order_items;
-drop table staff;
-drop table payments;
-drop table orders;
-drop table restaurant_tables;
-drop table menu_item_ingredients;
-drop table inventory;
-drop table feedback;
-drop table users;
-drop table menu_items;
-drop table categories;
-
---dropping sequence
-DROP SEQUENCE audit_log_seq;
-DROP SEQUENCE feedback_seq;
-DROP SEQUENCE menu_item_ingredients_seq;
-DROP SEQUENCE inventory_seq;
-DROP SEQUENCE payments_seq;
-DROP SEQUENCE order_items_seq;
-DROP SEQUENCE orders_seq;
-DROP SEQUENCE staff_seq;
-DROP SEQUENCE restaurant_tables_seq;
-DROP SEQUENCE menu_items_seq;
-DROP SEQUENCE categories_seq;
-DROP SEQUENCE users_seq;
-
 CREATE TABLE users (
     id          NUMBER PRIMARY KEY,
     name        VARCHAR2(100) NOT NULL,
