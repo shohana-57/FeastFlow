fe6042ecdf6c9e94a1b904c3f7aa6ed27c8eb8d7
Shohana Akter Rabina <shohana592059@gmail.com>
2026-06-27 19:20:09 +0600

    routing controller

--- feastflow_web/routes/web.php
+++ feastflow_web/routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
 
-Route::get('/test-db', function () {
-    $result = DB::select('SELECT * FROM users');
-    return response()->json($result);
-});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\MenuController;
+
+Route::get('/menu', [MenuController::class, 'index']);
